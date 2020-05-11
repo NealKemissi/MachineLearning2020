6e57ae51b82029cf2de47662b1f5934ec0070e3a
NealKemissi <neal.k@hotmail.fr>
2020-05-11 18:01:06 +0200

    signature methode (only in lib)

--- lib/main.cpp
+++ lib/main.cpp
@@ -12,4 +12,20 @@ extern "C" {
     DLLEXPORT int my_mul(int x, int y) {
         return x * y;
     }
+
+    // TODO : retourner Eigen np.ndarray
+    DLLEXPORT int linear_model_create(int input_dim) {
+        return 0;
+    }
+
+    DLLEXPORT double linear_model_predict_regression() {
+        return 0;
+    }
+
+    DLLEXPORT double linear_model_predict_classification() {
+        return 0;
+    }
+
+    DLLEXPORT void linear_model_train_classification() {
+    }
 }
