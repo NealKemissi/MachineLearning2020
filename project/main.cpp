cf4ddd75ab6ee733fe23b7c59dff7760a048b0a6
NealKemissi <neal.k@hotmail.fr>
2020-05-12 11:31:57 +0200

    add signature in main project

--- project/main.cpp
+++ project/main.cpp
@@ -7,7 +7,19 @@
 #include <iostream>
 
 extern "C" {
-DLLEXPORT int my_add(int x, int y);
+    DLLEXPORT int my_add(int x, int y);
+    DLLEXPORT int * linear_model_create(int input_dim);
+    DLLEXPORT double linear_model_predict_regression(double* model, double* inputs, int inputs_size);
+    DLLEXPORT double linear_model_predict_classification(double* model, double* inputs, int inputs_size);
+    DLLEXPORT double linear_model_train_classification(double* model,
+        double** dataset_inputs,
+        int dataset_length,
+        int inputs_size,
+        double* dataset_expected_outputs,
+        int outputs_size,
+        int iterations_count,
+        bool should_plot_results,
+        float alpha = 10);
 }
 
 int main() {
